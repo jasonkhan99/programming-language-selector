c5c8c1a8394aa97579c2a4eb6f13fa6278c71d60
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-05 10:17:37 -0700

    fixed a missing # on the math id using console.log in devtools.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
     event.preventDefault();
     const age = parseInt($("#age").val());
     const gender = parseInt($("#gender").val());
-    const math = parseInt($("math").val());
+    const math = parseInt($("#math").val());
     const media = parseInt($("input:radio[name=prefer]:checked").val());
+    const language = parseInt($("#language").val());
 
   });
 });
