000c8ee6b121dfa5e529b2c22a87b51c4f8ef6ce
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-05 11:41:42 -0700

    add js variables for all the survey questions, add branching logic for math question.

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,30 @@ $(document).ready(function() {
     const math = parseInt($("#math").val());
     const media = parseInt($("input:radio[name=prefer]:checked").val());
     const language = parseInt($("#language").val());
-    let result1; 
+
+    let resultAge;
+    let resultMath;
+    let result4;
+    let result5;
+
     if (age < 18) {
-      result1 = 1
+      resultAge = 1
     } else if (18 <= age && age <= 30) {
-      result1 = 2
+      resultAge = 4
     } else if (30 < age) {  
-      result1 = 3
+      resultAge = 7
     } else { 
-      alert("Please enter a number");
+      alert("Please enter a numerical value");
     }
-    $("#output").text(result1);
+
+    if (math === 4) {
+      resultMath = 7
+    } else if (math < 4 || math > 4) {
+      resultMath = 1
+    } else {
+      alert("Please enter a numerical value");
+    }
+
+    $("#output").text(resultMath);
   });
 });
