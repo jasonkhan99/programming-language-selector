fc52ac2fd2b02f2d649c404c6ff06dc330a4af44
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-05 11:10:03 -0700

    fix branching logic of if else and add output text function.

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
     const math = parseInt($("#math").val());
     const media = parseInt($("input:radio[name=prefer]:checked").val());
     const language = parseInt($("#language").val());
-    const result1
-    if (age > 18) {
+    let result1; 
+    if (age < 18) {
       result1 = 1
-    } else if (age === NaN) { 
-      alert("Please enter a number");
-    } else if (18 <= age <= 30) {
+    } else if (18 <= age && age <= 30) {
       result1 = 2
-    } else if (age > 30) {
+    } else if (30 < age) {  
       result1 = 3
+    } else { 
+      alert("Please enter a number");
     }
-
+    $("#output").text(result1);
   });
 });
