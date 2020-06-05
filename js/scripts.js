ae864cde2e95c6588138a43b8aebefadad7df5e9
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-05 10:01:29 -0700

    add function to gather input from 4th question and pass to variable, add a parse Int function to question 2.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $("form#codeSelector").submit(function() {
     event.preventDefault();
     const age = parseInt($("#age").val());
-    const gender = $("#gender").val();
+    const gender = parseInt($("#gender").val());
     const math = parseInt($("math").val());
+    const media = parseInt($("input:radio[name=prefer]:checked").val());
 
   });
 });
